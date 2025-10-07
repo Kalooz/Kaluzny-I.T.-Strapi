6f059484f4d2c11db162c330045872160cf89d39
Michal Kaluzny <mkaluzny17@gmail.com>
2025-10-07 14:45:53 -0500

    Add new content types and controllers for contact info, navigation links, portfolios, and portfolio tags; update TypeScript definitions

---
+++ src/api/portfolio-tag/controllers/portfolio-tag.ts
@@ -0,0 +1,7 @@
+/**
+ * portfolio-tag controller
+ */
+
+import { factories } from '@strapi/strapi'
+
+export default factories.createCoreController('api::portfolio-tag.portfolio-tag');
---
+++ src/api/portfolio-tag/routes/portfolio-tag.ts
@@ -0,0 +1,7 @@
+/**
+ * portfolio-tag router
+ */
+
+import { factories } from '@strapi/strapi';
+
+export default factories.createCoreRouter('api::portfolio-tag.portfolio-tag');
